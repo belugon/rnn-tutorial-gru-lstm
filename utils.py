609bcbfab502872653723797c3205f4211f8cb45
belugon <belugon@outlook.com>
2016-11-18 11:43:24 -0800

    python3 compatible char-by-char lstm training

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+# /usr/bin/env python
 
 import csv
 import itertools
@@ -10,56 +10,32 @@ import operator
 import io
 import array
 from datetime import datetime
-from gru_theano import GRUTheano
+from lstm_theano import LSTMTheano
 
-SENTENCE_START_TOKEN = "SENTENCE_START"
-SENTENCE_END_TOKEN = "SENTENCE_END"
-UNKNOWN_TOKEN = "UNKNOWN_TOKEN"
+CHUNK_SIZE = 128
 
-def load_data(filename="data/reddit-comments-2015-08.csv", vocabulary_size=2000, min_sent_characters=0):
+def load_data(filename="data/reddit-comments-2015-08.csv", vocabulary_size=2000, min_sent_characters=2):
 
-    word_to_index = []
-    index_to_word = []
-
-    # Read the data and append SENTENCE_START and SENTENCE_END tokens
-    print("Reading CSV file...")
+    chunks = []
+    # Read the data
+    print("Reading file...")
     with open(filename, 'rt') as f:
         reader = csv.reader(f, skipinitialspace=True)
-        reader.next()
+        next(reader)
         # Split full comments into sentences
-        sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode("utf-8").lower()) for x in reader])
+        sentences = itertools.chain(*[nltk.sent_tokenize(x[0]) for x in reader])
         # Filter sentences
         sentences = [s for s in sentences if len(s) >= min_sent_characters]
-        sentences = [s for s in sentences if "http" not in s]
-        # Append SENTENCE_START and SENTENCE_END
-        sentences = ["%s %s %s" % (SENTENCE_START_TOKEN, x, SENTENCE_END_TOKEN) for x in sentences]
-    print("Parsed %d sentences." % (len(sentences)))
+        sentences = [s.encode('ascii', 'ignore') for s in sentences if "http" not in s]
+        chunks = [[c for c in sentence] for sentence in sentences]
 
-    # Tokenize the sentences into words
-    tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
-
-    # Count the word frequencies
-    word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
-    print("Found %d unique words tokens." % len(word_freq.items()))
-
-    # Get the most common words and build index_to_word and word_to_index vectors
-    vocab = sorted(word_freq.items(), key=lambda x: (x[1], x[0]), reverse=True)[:vocabulary_size-2]
-    print("Using vocabulary size %d." % vocabulary_size)
-    print("The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1]))
-
-    sorted_vocab = sorted(vocab, key=operator.itemgetter(1))
-    index_to_word = ["<MASK/>", UNKNOWN_TOKEN] + [x[0] for x in sorted_vocab]
-    word_to_index = dict([(w, i) for i, w in enumerate(index_to_word)])
-
-    # Replace all words not in our vocabulary with the unknown token
-    for i, sent in enumerate(tokenized_sentences):
-        tokenized_sentences[i] = [w if w in word_to_index else UNKNOWN_TOKEN for w in sent]
+    print("Parsed %d sentence." % (len(chunks)))
 
     # Create the training data
-    X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
-    y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
+    X_train = np.asarray([np.asarray(chunk[:-1], dtype=np.int8) for chunk in chunks])
+    y_train = np.asarray([np.asarray(chunk[1:], dtype=np.int8) for chunk in chunks])
 
-    return X_train, y_train, word_to_index, index_to_word
+    return X_train, y_train
 
 
 def train_with_sgd(model, X_train, y_train, learning_rate=0.001, nepoch=20, decay=0.9,
@@ -84,13 +60,13 @@ def save_model_parameters_theano(model, outfile):
         V=model.V.get_value(),
         b=model.b.get_value(),
         c=model.c.get_value())
-    print "Saved model parameters to %s." % outfile
+    print("Saved model parameters to %s." % outfile)
 
-def load_model_parameters_theano(path, modelClass=GRUTheano):
+def load_model_parameters_theano(path, modelClass=LSTMTheano):
     npzfile = np.load(path)
     E, U, W, V, b, c = npzfile["E"], npzfile["U"], npzfile["W"], npzfile["V"], npzfile["b"], npzfile["c"]
     hidden_dim, word_dim = E.shape[0], E.shape[1]
-    print "Building model model from %s with hidden_dim=%d word_dim=%d" % (path, hidden_dim, word_dim)
+    print("Building model model from %s with hidden_dim=%d word_dim=%d" % (path, hidden_dim, word_dim))
     sys.stdout.flush()
     model = modelClass(word_dim, hidden_dim=hidden_dim)
     model.E.set_value(E)
@@ -113,7 +89,7 @@ def gradient_check_theano(model, x, y, h=0.001, error_threshold=0.01):
         # Get the actual parameter value from the mode, e.g. model.W
         parameter_T = operator.attrgetter(pname)(model)
         parameter = parameter_T.get_value()
-        print "Performing gradient check for parameter %s with size %d." % (pname, np.prod(parameter.shape))
+        print("Performing gradient check for parameter %s with size %d." % (pname, np.prod(parameter.shape)))
         # Iterate over each element of the parameter matrix, e.g. (0,0), (0,1), ...
         it = np.nditer(parameter, flags=['multi_index'], op_flags=['readwrite'])
         while not it.finished:
@@ -136,43 +112,46 @@ def gradient_check_theano(model, x, y, h=0.001, error_threshold=0.01):
             relative_error = np.abs(backprop_gradient - estimated_gradient)/(np.abs(backprop_gradient) + np.abs(estimated_gradient))
             # If the error is to large fail the gradient check
             if relative_error > error_threshold:
-                print "Gradient Check ERROR: parameter=%s ix=%s" % (pname, ix)
-                print "+h Loss: %f" % gradplus
-                print "-h Loss: %f" % gradminus
-                print "Estimated_gradient: %f" % estimated_gradient
-                print "Backpropagation gradient: %f" % backprop_gradient
-                print "Relative Error: %f" % relative_error
+                print("Gradient Check ERROR: parameter=%s ix=%s" % (pname, ix))
+                print("+h Loss: %f" % gradplus)
+                print("-h Loss: %f" % gradminus)
+                print("Estimated_gradient: %f" % estimated_gradient)
+                print("Backpropagation gradient: %f" % backprop_gradient)
+                print("Relative Error: %f" % relative_error)
                 return 
             it.iternext()
-        print "Gradient check for parameter %s passed." % (pname)
+        print("Gradient check for parameter %s passed." % (pname))
 
 
-def print_sentence(s, index_to_word):
-    sentence_str = [index_to_word[x] for x in s[1:-1]]
-    print(" ".join(sentence_str))
+def print_sentence(s):
+    print("".join([chr(c) for c in s]))
     sys.stdout.flush()
 
-def generate_sentence(model, index_to_word, word_to_index, min_length=5):
-    # We start the sentence with the start token
-    new_sentence = [word_to_index[SENTENCE_START_TOKEN]]
-    # Repeat until we get an end token
-    while not new_sentence[-1] == word_to_index[SENTENCE_END_TOKEN]:
-        next_word_probs = model.predict(new_sentence)[-1]
-        samples = np.random.multinomial(1, next_word_probs)
-        sampled_word = np.argmax(samples)
-        new_sentence.append(sampled_word)
-        # Seomtimes we get stuck if the sentence becomes too long, e.g. "........" :(
-        # And: We don't want sentences with UNKNOWN_TOKEN's
-        if len(new_sentence) > 100 or sampled_word == word_to_index[UNKNOWN_TOKEN]:
+def generate_sentence(model, min_length=20):
+    try:
+        # We start the sentence with the start token
+        new_sentence = [ord('.')]
+        # Repeat until we get an end token
+        while new_sentence[-1] != ord('.') or len(new_sentence) == 1:
+            next_word_probs = model.predict(new_sentence)[-1]
+            samples = np.random.multinomial(1, next_word_probs)
+            sampled_word = np.argmax(samples)
+            new_sentence.append(sampled_word)
+            # Seomtimes we get stuck if the sentence becomes too long, e.g. "........" :(
+            # And: We don't want sentences with UNKNOWN_TOKEN's
+            if len(new_sentence) > 200:
+                return None
+    except ValueError:
+            print("ValueError while generating sentence, skipping")
             return None
     if len(new_sentence) < min_length:
         return None
     return new_sentence
 
-def generate_sentences(model, n, index_to_word, word_to_index):
+def generate_sentences(model, n):
     for i in range(n):
         sent = None
         while not sent:
-            sent = generate_sentence(model, index_to_word, word_to_index)
-        print_sentence(sent, index_to_word)
+            sent = generate_sentence(model)
+        print_sentence(sent)
 
